286c1ff9c8d06316f2af2f00ae7bbd4b6bb09987
annalug <annalgs30@gmail.com>
2024-06-24 11:19:55 -0300

    adjusting app file

--- app.py
+++ app.py
@@ -18,6 +18,7 @@ from dotenv import load_dotenv
 load_dotenv()
 data_path = os.getenv('DATA_PATH')
 
+
 with open(f'{data_path}/config.yaml') as file:
     config = yaml.load(file, Loader=SafeLoader)
 
